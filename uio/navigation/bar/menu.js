81d66de93e45e6db054e88fd025792582021969b
dece24 <dece24@76c85f56-e4dc-11de-85bf-c3887d924cc8>
2010-02-16 08:33:50 +0000

    Bar Navigation. 1차 메뉴 포커스 아웃할 때 .active 클래스 제거.

--- uio/navigation/bar/menu.js
+++ uio/navigation/bar/menu.js
@@ -42,6 +42,7 @@ jQuery(function($){
 	}
 	menu.mouseleave(hide_this);
 	li_list.find('div.sub>ul').mouseleave(hide_this);
+	li_list.find('>a').focusout(hide_this);
 	li_list.find('div.sub li:last-child>a').focusout(hide_this);
 
 	//icon
