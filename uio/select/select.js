4bda3f551dd2ca4eb3b1f0a1bb0d1ec2ebb3c5c9
dece24 <dece24@76c85f56-e4dc-11de-85bf-c3887d924cc8>
2010-02-16 20:49:00 +0000

    select ui 선택된 텍스트 표시하기 완료. (IE 디버깅 남음)

--- uio/select/select.js
+++ uio/select/select.js
@@ -1,17 +1,39 @@
 jQuery(function($){
-	$('button.selected')
-		.click(function(){
-			var selectObj = this.parentNode;
-			if (selectObj.className == 'select') {
-				selectObj.className = 'select open';
-			} else {
-				selectObj.className = 'select';
-			}
-		})
-		.parent()
-		.find('>ul')
-		.mouseup(function(){
-			var selectObj = this.parentNode;
+	
+	var select_ui = $('div.select');
+	var button_ui = $('button.selected');
+	
+	function show_option(){
+		var selectObj = this.parentNode;
+		if (selectObj.className == 'select') {
+			selectObj.className = 'select open';
+		} else {
 			selectObj.className = 'select';
-		});
+		}
+	}
+	button_ui.click(show_option);
+	
+	function set_label(){
+		var t = $(this);
+		var v = t.next('label').text();
+		t.parent().parent().prev(button_ui).text('');
+		t.parent().parent().prev(button_ui).append(v);
+	}
+	select_ui.find('>ul>li>input').change(set_label).focus(set_label);
+		
+	function set_anchor(){
+		var t = $(this);
+		var v = t.text();
+		t.parent().parent().prev(button_ui).text('');
+		t.parent().parent().prev(button_ui).append(v);
+	}
+	select_ui.find('>ul>li>a').click(set_anchor);
+			
+	function hide_option(){
+		var selectObj = this.parentNode;
+		selectObj.className = 'select';
+	}
+	
+	select_ui.find('>ul').mouseup(hide_option);
+	
 });
