a5c7a8e1c6549ebb500fa1c34a644445231247d1
dece24 <dece24@76c85f56-e4dc-11de-85bf-c3887d924cc8>
2010-02-18 17:05:06 +0000

    마크업 구조와 동작을 select와 일치하도록 완전히 변경. FF, CR 키보드+마우스 관련 버그 있음.

--- trunk/uio/select/select.js
+++ trunk/uio/select/select.js
@@ -1,41 +1,61 @@
 jQuery(function($){
 	
-	var select_ui = $('div.select');
-	var button_ui = $('button.selected');
+	// Common
+	var select_root = $('div.select');
+	var select_value = $('.myValue');
+	var select_a = $('div.select>ul>li>a');
+	var select_input = $('div.select>ul>li>input[type=radio]');
+	var select_label = $('div.select>ul>li>label');
 	
+	// Default Value
+	var default_value = $('div.select>ul>li:first-child>label').text();
+	$('div.myValue').append(default_value);
+	
+	// Line
+	select_value.focusin(function(){$(this).addClass('outLine');});
+	select_value.focusout(function(){$(this).removeClass('outLine');});
+	select_input.focusin(function(){$(this).parents('div.select').children('div.myValue').addClass('outLine');});
+	select_input.focusout(function(){$(this).parents('div.select').children('div.myValue').removeClass('outLine');});
+	
+	// Show
 	function show_option(){
-		$(this).parents('div.select:first').addClass('open');
+		$(this).parents('div.select:first').toggleClass('open');
 	}
-	button_ui.mouseover(show_option).click(show_option);
 	
-	function list_hover(){
-		$(this).parents('li').toggleClass('hover');
+	// Hover
+	function i_hover(){
+		$(this).parents('ul:first').children('li').removeClass('hover');
+		$(this).parents('li:first').toggleClass('hover');
 	}
-
-	function set_label(){
-		var v = $(this).next('label').text();
-		$(this).parents('ul').prev(button_ui).text('').append(v);
-	}
-	select_ui.find('>ul>li>input').change(set_label).focus(set_label);
-	select_ui.find('>ul>li>label').hover(list_hover);
-		
-	function set_anchor(){
-		var v = $(this).text();
-		$(this).parents('ul').prev(button_ui).text('').append(v);
-	}
-	select_ui.find('>ul>li>a').click(set_anchor).click(hide_option).hover(list_hover);
-	select_ui.find('>ul>li>a').focus(list_hover);
-			
+	
+	// Hide
 	function hide_option(){
 		var t = $(this);
 		setTimeout(function(){
 			t.parents('div.select:first').removeClass('open');
 		}, 1);
 	}
-	select_ui.find('>ul').mouseup(hide_option);
+	
+	// Set Input
+	function set_label(){
+		var v = $(this).next('label').text();
+		$(this).parents('ul:first').prev(select_value).text('').append(v);
+		$(this).parents('ul:first').prev(select_value).addClass('selected');
+	}
+	
+	// Set Anchor
+	function set_anchor(){
+		var v = $(this).text();
+		$(this).parents('ul:first').prev(select_value).text('').append(v);
+		$(this).parents('ul:first').prev(select_value).addClass('selected');
+	}
 
-	select_ui.mouseleave(function(){
-		$(this).removeClass('open');
-	});
-
+	select_value.mouseover(show_option).click(show_option);
+	select_root.removeClass('open');
+	select_root.mouseleave(function(){$(this).removeClass('open');});
+	select_a.click(set_anchor).click(hide_option).focus(i_hover).hover(i_hover);
+	select_input.change(set_label);
+	select_label.hover(i_hover).click(hide_option);
+	
+			
 });
