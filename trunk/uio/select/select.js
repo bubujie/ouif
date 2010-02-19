85c52a1d6afd86253dee88ee0afca3737f723b00
dece24 <dece24@76c85f56-e4dc-11de-85bf-c3887d924cc8>
2010-02-19 10:36:30 +0000

    개선.

--- trunk/uio/select/select.js
+++ trunk/uio/select/select.js
@@ -56,6 +56,5 @@ jQuery(function($){
 	select_a.click(set_anchor).click(hide_option).focus(i_hover).hover(i_hover);
 	select_input.change(set_label).focus(set_label);
 	select_label.hover(i_hover).click(hide_option);
-	
 			
 });
