a6cdb7c32aef40ced0e1a5a24e559ebaddffe8a6
dece24 <dece24@76c85f56-e4dc-11de-85bf-c3887d924cc8>
2010-02-23 09:23:49 +0000

    jQuery 낮은 버전을 고려한 변경.

--- trunk/uio/navigation/bar/menu.js
+++ trunk/uio/navigation/bar/menu.js
@@ -1,15 +1,15 @@
 jQuery(function($){
-
+	
+	// Menu
 	var menu = $('div.menu');
 	var major = $('div.major');
-	var li_list = major.find('li');
+	var li_list = major.find('>ul>li');
 	
 	// Selected
 	function onselectmenu(){
-		var t = $(this);
 		var myclass = [];
 		
-		t.parentsUntil('div.major').filter('li').each(function(){
+		$(this).parent('li').each(function(){
 			myclass.push( $(this).attr('class') );
 		});
 		
@@ -17,38 +17,54 @@ jQuery(function($){
 		if (!major.hasClass(myclass)) major.attr('class','major').addClass(myclass);
 	}
 	
-	// Show
-	function show_this(){
-		var t = $(this);
-		
+	// Show Menu
+	function show_menu(){
 		li_list.removeClass('active');
-		t.parentsUntil('div.major').filter('li').addClass('active');
+		$(this).parent('li').addClass('active');
 		
 		// IE7 or IE7 documentMode bug fix
 		if($.browser.msie) {
 			var v = document.documentMode || parseInt($.browser.version);
 
 			if (v == 7) {
-				var div = t.parentsUntil('div.sub').eq(-1);
-				div.css('width', '').css('width', div.width()+'px');
+				var sub = $(this).next('div.sub').eq(-1);
+				sub.css('width', '').css('width', sub.width()+'px');
 			}
 		}
 	}
-	li_list.find('>a').click(onselectmenu).mouseover(show_this).focus(show_this);
+	li_list.find('>a').click(onselectmenu).mouseover(show_menu).focus(show_menu);
 	
-	// Hide
+	// Hide Menu
 	function hide_menu(){
 		li_list.removeClass('active');
 	}
 	menu.mouseleave(hide_menu);
 	li_list.find('div.sub>ul').mouseleave(hide_menu);
-	li_list.find('div.sub li:last-child>a').focusout(hide_menu);
-
-	// icon
+	
+	//icon
 	major.find('div.sub').prev('a').find('>span').append('<span class="i"></span>');
 	
-	// Focus Out
-	$('*:not(".menu a")').focus(hide_menu);
+	// Aside
+	var aside_li = $('.menu>.inset>.aside>ul>li');
+	var aside_a = $('.menu>.inset>.aside>ul>li>a');
+
+	// Show Aside
+	function show_aside(){
+		li_list.removeClass('active');
+		aside_li.removeClass('active');
+		$(this).parent('li').addClass('active');
+	}	
+	aside_a.mouseover(show_aside).focus(show_aside);
+	
+	// Hide Aside
+	function hide_aside(){
+		aside_li.removeClass('active');
+	}	
+	menu.mouseleave(hide_aside);
+	aside_li.find('div.sub>ul').mouseleave(hide_aside);
+
+	// Hide Menu & Aside
+	$('*:not(".menu a")').focus(hide_menu).focus(hide_aside);
 	
 });
 
