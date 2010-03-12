76ef42e003bb706995a4dd22703cad7f752a4015
dece24 <dece24@76c85f56-e4dc-11de-85bf-c3887d924cc8>
2010-03-12 09:56:24 +0000

    datePicker

---
+++ trunk/uio/calendar/datePicker/datePicker.js
@@ -0,0 +1,12 @@
+$(function() {
+	$('.datepicker')
+	.datepicker({
+		dateFormat:'yy-mm-dd', 
+		showAnim:'fadeIn', // show|slideDown|fadeIn
+		changeYear:true,
+		showOtherMonths:true,
+		changeMonth:true, 
+		//selectOtherMonths: true,
+		//showButtonPanel:true
+	})
+});
