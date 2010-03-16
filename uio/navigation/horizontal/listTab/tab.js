89face62247ad52ba93ef9ff9f9e79a2bc816253
dece24 <dece24@76c85f56-e4dc-11de-85bf-c3887d924cc8>
2010-03-16 03:03:46 +0000

    tab 자바스크립트 미지원 환경에 대한 개선.

--- uio/navigation/horizontal/listTab/tab.js
+++ uio/navigation/horizontal/listTab/tab.js
@@ -1,15 +1,13 @@
 jQuery(function($){
-
 	var tab = $('div.tab');
+	tab.removeClass('jsOff');
 	tab.css('height', tab.find('>ul>li>ul:visible').height()+40);
-
 	function onSelectTab(){
 		var t = $(this);
 		var myClass = t.parent('li').attr('class');
 		t.parents('div.tab:first').attr('class', 'tab '+myClass);
 		tab.css('height', t.next('ul').height()+40);
 	}
-	
 	tab.find('>ul>li>a').click(onSelectTab).focus(onSelectTab);
 });
 
