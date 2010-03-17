e758c93dacc872c6ad38a2f4496f5c5e4c0d11d0
dece24 <dece24@76c85f56-e4dc-11de-85bf-c3887d924cc8>
2010-03-17 08:34:12 +0000

    navigation/horizontal 클래스 이름을 다르게 처리

--- uio/navigation/horizontal/linedTab/linedTab.js
+++ uio/navigation/horizontal/linedTab/linedTab.js
@@ -1,14 +1,14 @@
 jQuery(function($){
-	var tab = $('div.tab');
+	var tab = $('.linedTab');
 	tab.removeClass('jsOff');
 	function onSelectTab(){
 		var t = $(this);
 		var myclass = [];
-		t.parentsUntil('div.tab').filter('li').each(function(){
+		t.parentsUntil('.linedTab:first').filter('li').each(function(){
 			myclass.push( $(this).attr('class') );
 		});
 		myclass = myclass.join(' ');
-		if (!tab.hasClass(myclass)) tab.attr('class','tab').addClass(myclass);
+		if (!tab.hasClass(myclass)) tab.attr('class','linedTab').addClass(myclass);
 	}
 	tab.find('li>a').click(onSelectTab).focus(onSelectTab);
 });
