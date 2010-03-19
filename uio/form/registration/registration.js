23c4a01a9c5cc1693083856b92178712654bad50
dece24 <dece24@76c85f56-e4dc-11de-85bf-c3887d924cc8>
2010-03-19 08:28:16 +0000

    registration form

---
+++ uio/form/registration/registration.js
@@ -0,0 +1,17 @@
+jQuery(function($){
+	// Input Clear
+	var iText = $('.regist .iLabel').next('.iText');
+	$('.regist .iLabel').css('position','absolute');
+	iText
+		.focus(function(){
+			$(this).prev('.iLabel').css('visibility','hidden');
+		})
+		.blur(function(){
+			if($(this).val() == ''){
+				$(this).prev('.iLabel').css('visibility','visible');
+			} else {
+				$(this).prev('.iLabel').css('visibility','hidden');
+			}
+		})
+		.blur();
+});
