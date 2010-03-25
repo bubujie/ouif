234145217dcfe6bc04a897561db123263707d5da
dece24 <dece24@76c85f56-e4dc-11de-85bf-c3887d924cc8>
2010-03-25 19:37:05 +0000

    faq update

--- uio/list/faq/faq.js
+++ uio/list/faq/faq.js
@@ -2,14 +2,18 @@ jQuery(function(){
 	
 	var article = $('.faq .article');
 	article.addClass('hide');
+	article.find('.a').slideUp(100);
 	
 	$('.faq .article .trigger').click(function(){
 		var myArticle = $(this).parents('.article:first');
 		if(myArticle.hasClass('hide')){
 			article.addClass('hide').removeClass('show'); // 아코디언 효과를 원치 않으면 이 라인을 지우세요
+			article.find('.a').slideUp(100); // 아코디언 효과를 원치 않으면 이 라인을 지우세요
 			myArticle.removeClass('hide').addClass('show');
+			myArticle.find('.a').slideDown(100);
 		} else {
 			myArticle.removeClass('show').addClass('hide');
+			myArticle.find('.a').slideUp(100);
 		}
 	});
 	
@@ -17,8 +21,10 @@ jQuery(function(){
 		var hidden = $('.faq .article.hide').length;
 		if(hidden > 0){
 			article.removeClass('hide').addClass('show');
+			article.find('.a').slideDown(100);
 		} else {
 			article.removeClass('show').addClass('hide');
+			article.find('.a').slideUp(100);
 		}
 	});
 	
