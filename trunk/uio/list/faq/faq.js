58122252c89e6b7dec8534753a4c7291f585ba44
dece24 <dece24@76c85f56-e4dc-11de-85bf-c3887d924cc8>
2010-03-25 14:55:41 +0000

    qna delete / faq add

--- trunk/uio/list/faq/faq.js
+++ trunk/uio/list/faq/faq.js
@@ -1,9 +1,9 @@
 jQuery(function(){
 	
-	var article = $('.qna .article');
+	var article = $('.faq .article');
 	article.addClass('hide');
 	
-	$('.qna .article .trigger').click(function(){
+	$('.faq .article .trigger').click(function(){
 		var myArticle = $(this).parents('.article:first');
 		if(myArticle.hasClass('hide')){
 			article.addClass('hide').removeClass('show'); // 아코디언 효과를 원치 않으면 이 라인을 지우세요
@@ -13,8 +13,8 @@ jQuery(function(){
 		}
 	});
 	
-	$('.qna .hgroup .trigger').click(function(){
-		var hidden = $('.qna .article.hide').length;
+	$('.faq .hgroup .trigger').click(function(){
+		var hidden = $('.faq .article.hide').length;
 		if(hidden > 0){
 			article.removeClass('hide').addClass('show');
 		} else {
