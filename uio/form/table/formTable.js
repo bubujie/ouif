601da133288c0036e59553611d89c2e61c06059b
dece24 <dece24@76c85f56-e4dc-11de-85bf-c3887d924cc8>
2010-04-06 13:53:23 +0000

    update (clear, form, ul, ol, image list, skip)

--- uio/form/table/formTable.js
+++ uio/form/table/formTable.js
@@ -1,6 +1,28 @@
 jQuery(function(){
 	// Help Toggle
 	$('.item>.iHelp').click(function(){
-		$(this).parent('.item').find('.desc').toggleClass('hide');
+		$(this).parent('.item').find('.iDesc').toggleClass('hide');
 	});
+	// Input Clear
+	var iText = $('.item>.iLabel').next('.iText');
+	$('.item>.iLabel').css('position','absolute');
+	iText
+		.focus(function(){
+			$(this).prev('.iLabel').css('visibility','hidden');
+		})
+		.blur(function(){
+			if($(this).val() == ''){
+				$(this).prev('.iLabel').css('visibility','visible');
+			} else {
+				$(this).prev('.iLabel').css('visibility','hidden');
+			}
+		})
+		.change(function(){
+			if($(this).val() == ''){
+				$(this).prev('.iLabel').css('visibility','visible');
+			} else {
+				$(this).prev('.iLabel').css('visibility','hidden');
+			}
+		})
+		.blur();
 });
