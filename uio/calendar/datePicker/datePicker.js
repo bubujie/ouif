2633136545b4e13c4f3b4fe3dbe2e82fe2437da8
dece24 <dece24@76c85f56-e4dc-11de-85bf-c3887d924cc8>
2010-05-04 00:36:33 +0000

    datePicker.js update

--- uio/calendar/datePicker/datePicker.js
+++ uio/calendar/datePicker/datePicker.js
@@ -1,12 +1,10 @@
-$(function() {
+jQuery(function($) {
 	$('.datepicker')
 	.datepicker({
 		dateFormat:'yy-mm-dd', 
 		showAnim:'fadeIn', // show|slideDown|fadeIn
 		changeYear:true,
 		showOtherMonths:true,
-		changeMonth:true, 
-		//selectOtherMonths: true,
-		//showButtonPanel:true
-	})
+		changeMonth:true
+	});
 });
