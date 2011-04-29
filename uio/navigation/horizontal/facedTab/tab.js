61eb006a56346b73b7e5530ed970b5e7abcb1d0e
dece24 <dece24@76c85f56-e4dc-11de-85bf-c3887d924cc8>
2011-04-29 09:54:15 +0000

    Tab Navigation JS Refactoring.

--- uio/navigation/horizontal/facedTab/tab.js
+++ uio/navigation/horizontal/facedTab/tab.js
@@ -1,25 +1,25 @@
 jQuery(function($){
-	// Tab Navigation
-	var tab = $('div.tab');
-	var tab_i = tab.find('>ul>li');
-	var tab_ii = tab.find('>ul>li>ul>li');
-	tab.removeClass('jx');
-	tab_i.find('>ul').css('display','none');
-	tab_i.find('>ul>li[class=active]').parents('li').attr('class','active');
-	tab.find('>ul>li[class=active]').find('>ul').css('display','block');
-	function tabMenuToggle(event){
+	// Faced Tab Navigation
+	var tab_face = $('div.tab.face');
+	var tab_face_i = tab_face.find('>ul>li');
+	var tab_face_ii = tab_face.find('>ul>li>ul>li');
+	tab_face.removeClass('jx');
+	tab_face_i.find('>ul').hide();
+	tab_face_i.find('>ul>li[class=active]').parents('li').attr('class','active');
+	tab_face.find('>ul>li[class=active]').find('>ul').show();
+	function faceTabMenuToggle(event){
 		var t = $(this);
-		tab_i.find('>ul').hide();
+		tab_face_i.find('>ul').hide();
 		t.next('ul').show();
-		tab_i.removeClass('active');
+		tab_face_i.removeClass('active');
 		t.parent('li').addClass('active');
 		return false;
 	}
-	function tabSubMenuActive(){
-		tab_ii.removeClass('active');
-		$(this).parent(tab_ii).addClass('active');
+	function faceTabSubMenuActive(){
+		tab_face_ii.removeClass('active');
+		$(this).parent(tab_face_ii).addClass('active');
 		return false;
 	}; 
-	tab_i.find('>a[href=#]').click(tabMenuToggle).focus(tabMenuToggle);
-	tab_ii.find('>a[href=#]').click(tabSubMenuActive).focus(tabSubMenuActive);
+	tab_face_i.find('>a[href=#]').click(faceTabMenuToggle).focus(faceTabMenuToggle);
+	tab_face_ii.find('>a[href=#]').click(faceTabSubMenuActive).focus(faceTabSubMenuActive);
 });
