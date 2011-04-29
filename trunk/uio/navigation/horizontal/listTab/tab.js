29d8d6cb72f0213269832346059f48a847ed1945
dece24 <dece24@76c85f56-e4dc-11de-85bf-c3887d924cc8>
2011-04-29 08:34:25 +0000

    Tab Navigation JS Refactoring.

--- trunk/uio/navigation/horizontal/listTab/tab.js
+++ trunk/uio/navigation/horizontal/listTab/tab.js
@@ -2,10 +2,8 @@ jQuery(function($){
 	// List Tab Navigation
 	var tab_list = $('div.tab.list');
 	var tab_list_i = tab_list.find('>ul>li');
-	var tab_list_ii = tab_list.find('>ul>li>ul>li');
 	tab_list.removeClass('jx');
 	tab_list_i.find('>ul').css('display','none');
-	tab_list_i.find('>ul>li[class=active]').parents('li').attr('class','active');
 	tab_list.find('>ul>li[class=active]').find('>ul').css('display','block');
 	tab_list.css('height', tab_list.find('>ul>li>ul:visible').height()+40);
 	function listTabMenuToggle(event){
@@ -17,11 +15,5 @@ jQuery(function($){
 		tab_list.css('height', t.next('ul').height()+40);
 		return false;
 	}
-	function listTabSubMenuActive(){
-		tab_list_ii.removeClass('active');
-		$(this).parent(tab_list_ii).addClass('active');
-		return false;
-	}; 
 	tab_list_i.find('>a[href=#]').click(listTabMenuToggle).focus(listTabMenuToggle);
-	tab_list_ii.find('>a[href=#]').click(listTabSubMenuActive).focus(listTabSubMenuActive);
 });
