d689d96c4e9bfa4454e29e211aaa8cf0806fdb20
dece24 <dece24@76c85f56-e4dc-11de-85bf-c3887d924cc8>
2011-05-06 17:10:38 +0000

    Navigation Update.

--- uio/navigation/vertical/tree/tree.js
+++ uio/navigation/vertical/tree/tree.js
@@ -1,22 +1,17 @@
 jQuery(function($){
-	var tree = $('.tree');
-	var togglePlus = '\<button type=\"button\" class=\"toggle plus\"\>+\<\/button\>';
-	var toggleMinus = '\<button type=\"button\" class=\"toggle minus\"\>-\<\/button\>';
-	
-	// defalt
-	tree.find('li>ul').css('display','none');
-	tree.find('ul>li:last-child').addClass('last');
-	tree.find('li>ul:hidden').parent('li').prepend(togglePlus);
-	tree.find('li>ul:visible').parent('li').prepend(toggleMinus);
-	
-	// active
-	tree.find('li.active').parents('li').addClass('open');
-	tree.find('li.open').parents('li').addClass('open');
-	tree.find('li.open>.toggle').text('-').removeClass('plus').addClass('minus');
-	tree.find('li.open>ul').slideDown(100);
-	
-	// click toggle
-	$('.tree .toggle').click(function(){
+	// Tree Navigation
+	var tNav = $('.tNav');
+	var tNavPlus = '\<button type=\"button\" class=\"tNavToggle plus\"\>+\<\/button\>';
+	var tNavMinus = '\<button type=\"button\" class=\"tNavToggle minus\"\>-\<\/button\>';
+	tNav.find('li>ul').css('display','none');
+	tNav.find('ul>li:last-child').addClass('last');
+	tNav.find('li>ul:hidden').parent('li').prepend(tNavPlus);
+	tNav.find('li>ul:visible').parent('li').prepend(tNavMinus);
+	tNav.find('li.active').addClass('open').parents('li').addClass('open');
+	tNav.find('li.open').parents('li').addClass('open');
+	tNav.find('li.open>.tNavToggle').text('-').removeClass('plus').addClass('minus');
+	tNav.find('li.open>ul').slideDown(100);
+	$('.tNav .tNavToggle').click(function(){
 		t = $(this);
 		t.parent('li').toggleClass('open');
 		if(t.parent('li').hasClass('open')){
@@ -26,6 +21,18 @@ jQuery(function($){
 			t.text('+').removeClass('minus').addClass('plus');
 			t.parent('li').find('>ul').slideUp(100);
 		}
+		return false;
 	});
-	
+	$('.tNav a[href=#]').click(function(){
+		t = $(this);
+		t.parent('li').toggleClass('open');
+		if(t.parent('li').hasClass('open')){
+			t.prev('button.tNavToggle').text('-').removeClass('plus').addClass('minus');
+			t.parent('li').find('>ul').slideDown(100);
+		} else {
+			t.prev('button.tNavToggle').text('+').removeClass('minus').addClass('plus');
+			t.parent('li').find('>ul').slideUp(100);
+		}
+		return false;
+	});
 });
