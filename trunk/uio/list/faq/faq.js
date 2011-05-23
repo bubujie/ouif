bb2e17769ee0eaeaaf0bfda269b7a0ef74fe45c9
dece24 <dece24@76c85f56-e4dc-11de-85bf-c3887d924cc8>
2011-05-23 15:07:07 +0000

    faq update

--- trunk/uio/list/faq/faq.js
+++ trunk/uio/list/faq/faq.js
@@ -1,15 +1,15 @@
 jQuery(function($){
 	// Frequently Asked Question
-	var article = $('.faq .article');
+	var article = $('.faq>.faqBody>.article');
 	article.addClass('hide');
 	article.find('.a').hide();
 	article.eq(0).removeClass('hide');
-	article.eq(0).find('.a').show(); // 첫 번째 항목을 열어 둠
-	$('.faq>ul>.article>.q>a').click(function(){
+	article.eq(0).find('.a').show();
+	$('.faq>.faqBody>.article>.q>a').click(function(){
 		var myArticle = $(this).parents('.article:first');
 		if(myArticle.hasClass('hide')){
-			article.addClass('hide').removeClass('show'); // 아코디언 효과를 원치 않으면 이 라인을 지움
-			article.find('.a').slideUp(100); // 아코디언 효과를 원치 않으면 이 라인을 지움
+			article.addClass('hide').removeClass('show');
+			article.find('.a').slideUp(100);
 			myArticle.removeClass('hide').addClass('show');
 			myArticle.find('.a').slideDown(100);
 		} else {
@@ -19,7 +19,7 @@ jQuery(function($){
 		return false;
 	});
 	$('.faq>.faqHeader>.showAll').click(function(){
-		var hidden = $('.faq .article.hide').length;
+		var hidden = $('.faq>.faqBody>.article.hide').length;
 		if(hidden > 0){
 			article.removeClass('hide').addClass('show');
 			article.find('.a').slideDown(100);
